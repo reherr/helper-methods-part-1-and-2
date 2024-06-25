462be8c1b588a86912bb10209a66e2a23c70eecd
Rebecca Herrera <133678924+reherr@users.noreply.github.com>
2024-06-25 17:43:43 +0000

    refactored new movie form

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -34,9 +34,10 @@ class MoviesController < ApplicationController
   end
 
   def create
-    @movie = Movie.new
-    @movie.title = params.fetch(:movie).fetch(:title)
-    @movie.description = params.fetch(:movie).fetch(:description)
+    movie_attributes = params.require(:movie).permit(:title, :description)
+    @movie = Movie.new(movie_attributes)
+    # @movie.title = params.fetch(:movie).fetch(:title)
+    # @movie.description = params.fetch(:movie).fetch(:description)
 
     if @movie.valid?
       @movie.save
